df2091c27ce12121f911775c654504e2fd5bf562
ZhongmouZhang <james.zm.zhang@gmail.com>
2019-02-18 22:37:01 -0800

    periodically

--- Python/src/Demo_ODBC_Test.py
+++ Python/src/Demo_ODBC_Test.py
@@ -1,6 +1,8 @@
 import pyodbc as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider
+
 
 # Some other example server values are
 # server = 'localhost\sqlexpress' # for a named instance
@@ -13,15 +15,34 @@ cnxn = pd.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER='+server+';DATA
 cursor = cnxn.cursor()
 
 #Call a stored procedure
-cursor.execute("{CALL [sp_load_vector](@work_serial=?, @stamp_begin=?)}", ("1", "193342")) 
+cursor.execute("{CALL [sp_load_vector](@work_serial=?, @stamp_begin=?)}", (101, 741000)) 
 rows = cursor.fetchall() 
 array = np.array(rows, float)
-
-fig = plt.gcf()
-fig.set_size_inches(100, 29)
+array_max_per_channel = np.max(array, axis=0)
+array_min_per_channel = np.min(array, axis=0)
 
 x = np.arange(0,10.0, 1/512)
-plt.plot(x, array[:,0])
+
+
+fig, ax = plt.subplots(19,1, figsize=(20,20), sharex=True, frameon=False)
+plt.subplots_adjust(bottom=0.10, top=0.97, left=0.11, right=0.98, hspace=0.37)
+
+
+
+
+# plt.yticks([])
+# ax[0].grid(False)
+# ax[18].grid(False)
+
+
+for i in range (19):
+    ax[i].axis([0, 10, array_min_per_channel[i], array_max_per_channel[i]], ylable='test') 
+    ax[i].plot(x,array[:,i])
+    ax[i].set_ylabel('C3_N C3-A1+A2',rotation=0, horizontalalignment='left') 
+    ax[i].yaxis.set_label_coords(-0.12,1.1)
+    
+    
+
 plt.show()
 
 #print (array[:,0])
--- Python/src/Demo_scrollable_plot.py
+++ Python/src/Demo_scrollable_plot.py
@@ -2,25 +2,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
 
-fig, ax = plt.subplots()
-plt.subplots_adjust(bottom=0.25)
+
+fig, ax = plt.subplots(19,1, figsize=(20,20))
+plt.subplots_adjust(bottom=0.16, top=0.99, left=0.09, right=0.97, hspace=0.35)
 
 t = np.arange(0.0, 100.0, 0.1)
-s = np.sin(2*np.pi*t)
-l, = plt.plot(t,s)
+s = np.sin(np.pi*t)
+ax[0].plot(t,s)
+ax[18].plot(t, 10*s)
 
 # axis = [x_min, x_max, y_min, y_max]
-plt.axis([20, 30, -1, 1])  
+plt.axis([0, 10, -1, 1])  
 
 axcolor = 'lightgoldenrodyellow'
 # axes rect = [left, bottom, width, height] ; the value is the percentage of the whole figure size
 axpos = plt.axes([0.2, 0.1, 0.65, 0.03], facecolor=axcolor)
 
-spos = Slider(axpos, 'Pos', 0, 90.0)
+spos = Slider(axpos, 'Pos', 0, 90, valstep=1)
 
 def update(val):
     pos = spos.val
-    ax.axis([pos,pos+10,-1,1])
+    ax[0].axis([pos,pos+10,-1,1])
+    ax[18].axis([pos,pos+10,-20,20])
+    # plt.axis([pos,pos+10,-2,2])
     fig.canvas.draw_idle()
 
 spos.on_changed(update)
