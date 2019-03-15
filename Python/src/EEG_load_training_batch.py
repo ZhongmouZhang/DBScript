84ed3f1a44633872689e999e4f7e75eaab19e7eb
ZhongmouZhang <james.zm.zhang@gmail.com>
2019-03-15 16:06:14 -0700

    Modification

--- Python/src/EEG_load_training_batch.py
+++ Python/src/EEG_load_training_batch.py
@@ -2,19 +2,14 @@ import pyodbc as pd
 import numpy as np
 
 
-def load_mini_batch (batch_number):
-    server = 'SDDBSSBX004' 
-    database = 'EEG_Data' 
-    cnxn = pd.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER='+server+';DATABASE='+database+';Trusted_Connection=Yes')
-    cursor = cnxn.cursor()
-
+def load_mini_batch_of_ten_second (cursor, batch_number):
     #Call a stored procedure
     id = 0
-    cursor.execute("{CALL sp_load_ten_second_training_sample (@batch_number=?, @id=?)}", (batch_number, id)) 
+    cursor.execute("{CALL sp_load_training_sample_of_ten_second (@batch_number=?, @id=?)}", (batch_number, id)) 
     id, tagged, _ = cursor.fetchone()
     cursor.nextset()
     rows=cursor.fetchall()
-    X = np.array(rows, float).reshape(97280,1)
+    X = np.array(rows, float).reshape((5120, 19, 1))
 
     if tagged == 'Y':
         Y = [[1]]
@@ -23,13 +18,45 @@ def load_mini_batch (batch_number):
 
 
     while tagged is not None:
-        cursor.execute("{CALL sp_load_a_training_sample (@batch_number=?, @id=?)}", (batch_number, id)) 
-        id, tagged, swapped = cursor.fetchone()
+        cursor.execute("{CALL sp_load_training_sample_of_ten_second (@batch_number=?, @id=?)}", (batch_number, id)) 
+        id, tagged, _ = cursor.fetchone()
         if tagged is not None:
             cursor.nextset()
             rows=cursor.fetchall()
-            array = np.array(rows, float).reshape(97280,1)
-            X = np.append(X,array, axis = 1)
+            array = np.array(rows, float).reshape((5120, 19, 1))
+            X = np.append(X,array, axis = 2)
+            if tagged == 'Y':
+                Y = np.append(Y,[[1]],axis=1)
+            else:
+                Y = np.append(Y,[[0]], axis = 1)
+
+    return (X,Y)
+
+
+def load_mini_batch_of_one_second (cursor, batch_number):
+
+    #Call a stored procedure
+    id = 0
+    cursor.execute("{CALL sp_load_training_sample_of_one_second (@batch_number=?, @id=?)}", (batch_number, id)) 
+    id, tagged, _ = cursor.fetchone()
+    cursor.nextset()
+    rows=cursor.fetchall()
+    X = np.array(rows, float).reshape((512, 19, 1))
+
+    if tagged == 'Y':
+        Y = [[1]]
+    else:
+        Y = [[0]]
+
+
+    while tagged is not None:
+        cursor.execute("{CALL sp_load_training_sample_of_one_second (@batch_number=?, @id=?)}", (batch_number, id)) 
+        id, tagged, _ = cursor.fetchone()
+        if tagged is not None:
+            cursor.nextset()
+            rows=cursor.fetchall()
+            array = np.array(rows, float).reshape((512, 19, 1))
+            X = np.append(X,array, axis = 2)
             if tagged == 'Y':
                 Y = np.append(Y,[[1]],axis=1)
             else:
